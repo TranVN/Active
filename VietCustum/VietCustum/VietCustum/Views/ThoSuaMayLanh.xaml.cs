c450a6147ff21dd998e27e390bb674c0779a20a6
ThongKieu <46147175+ThongKieu@users.noreply.github.com>
2020-10-20 17:23:57 +0700

    add file thodiennuoc thodienlanh thomoc thochongtham thosuamaylanh

---
+++ VietCustum/VietCustum/VietCustum/Views/ThoSuaMayLanh.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VietCustum.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ThoSuaMayLanh : ContentPage
+    {
+        public ThoSuaMayLanh()
+        {
+            InitializeComponent();
+        }
+    }
+}
