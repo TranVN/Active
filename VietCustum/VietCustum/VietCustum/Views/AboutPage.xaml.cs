439f3fae24b0b592acf96e4b7bca1bfe785354cd
TranVN <coppership1@gmail.com>
2020-10-20 16:22:19 +0700

    1

---
+++ VietCustum/VietCustum/VietCustum/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VietCustum.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
