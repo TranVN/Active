66bb10a3b8e6f88498ba66e5b0cd50a64da7f630
it <it@DESKTOP-51EE749>
2020-10-23 13:29:06 +0700

    1

--- VietCustum/VietCustum/VietCustum/AppShell.xaml.cs
+++ VietCustum/VietCustum/VietCustum/AppShell.xaml.cs
@@ -1,21 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using VietCustum.ViewModels;
 using VietCustum.Views;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace VietCustum
 {
-    public partial class AppShell : Xamarin.Forms.TabbedPage
+    public partial class AppShell : Xamarin.Forms.Shell
     {
         public AppShell()
         {
             InitializeComponent();
-            var pages = Children.GetEnumerator();
-            pages.MoveNext(); // First page
-            pages.MoveNext(); // Second page
-            CurrentPage = pages.Current;
+           
+        }
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+
+            await Shell.Current.GoToAsync("//LoginPage");
         }
 
+        private async void ButtonFabcon_Clicked(object sender, System.EventArgs e)
+        {
+            //await ((ImageButton)sender).ScaleTo(0.8, 50, Easing.Linear);
+            await Task.Delay(100);
+            //await Launcher.OpenAsync(new Uri("tel:0903532938"));
+            //await ((ImageButton)sender).ScaleTo(1, 50, Easing.Linear);
+        }
+
+
     }
+
+    
 }
